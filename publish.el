1080cb14d39dae0a0d75dfd22e0dc5457e60e1c2
Lowtroo <xieyuhuai@outlook.com>
2026-03-21 00:31:02 +0100

    we have index now

--- publish.el
+++ publish.el
@@ -1,25 +1,42 @@
 (require 'package)
 (package-initialize)
 (require 'ox-publish)
+
 (setq org-publish-project-alist
       '(
-	("Number Theory"
-         :base-directory "./contents/Number_Theory"
-         :publishing-directory "./public"
-         :publishing-function org-html-publish-to-html
-	 :recursive t
-    	 :with-title t
+	("index"
+	 :base-directory "./contents"
+	 :publishing-directory "./public"
+	 :publishing-function org-html-publish-to-html
+	 :recursive nil
+	 :with-title t
     	 :with-smart-quotes t
          :with-author t
          :with-creator t
+	 :with-toc nil
+	 :section-numbers nil
+	 )
+	("Number Theory"
+         :base-directory "./contents/Number_Theory"
+         :publishing-directory "./public/Number_Theory"
+	 :recursive nil
+	 :with-title t
+    	 :with-smart-quotes t
+	 :with-author t
+	 :with-creator t
          :with-toc t
-         :section-numbers t)
+         :section-numbers t
+	 :auto-sitemap t
+	 :sitemap-filename "sitemap.org"
+	 :sitemap-style list
+	 :sitemap-title ""
+	 )
         ;; ("site-static"
         ;;  :base-directory "~/pages/static"
         ;;  :publishing-directory "~/pages/public"
         ;;  :publishing-function org-publish-attachment)
         ("pages"
-         :components ("Number Theory"))
+         :components ("index" "Number Theory"))
 	)
       )
-(org-publish "pages" nil nil)
+(org-publish "pages" t nil)
