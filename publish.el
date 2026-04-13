ed6ea4692b474ef46a021ac51ee653d50cd97446
Lowtroo <xieyuhuai@outlook.com>
2026-04-13 18:39:40 +0200

    add new topic: cryptography

--- publish.el
+++ publish.el
@@ -10,6 +10,33 @@
 
 (setq org-publish-project-alist
       `(
+	("cryptography"
+         :base-directory "./contents/cryptography"
+         :publishing-directory "./public/cryptography"
+	 :html-doctype "html5"
+	 :html-html5-fancy t
+	 :html-head-include-default-style nil
+	 :html-head ,(my-inline-css "./static/solarized_light.css")
+	 :html-postamble t
+	 :html-postamble-format (("en" "<p class=\"author\">Author: %a</p>
+<p class=\"date\">Created on: %d</p>
+<p class=\"creator\">Powered by %c</p>"))
+	 
+	 :author "Lowtroo"
+	 :language "en"
+	 :recursive nil
+	 :with-title t
+    	 :with-smart-quotes t
+	 :with-author t
+	 :with-creator t
+         :with-toc t
+	 :with-date t
+         :section-numbers t
+	 :auto-sitemap t
+	 :sitemap-filename "sitemap.org"
+	 :sitemap-style list
+	 :sitemap-title ""
+	 )
 	("data_networks"
          :base-directory "./contents/data_networks"
          :publishing-directory "./public/data_networks"
@@ -168,7 +195,7 @@
         ;;  :publishing-directory "~/pages/public"
         ;;  :publishing-function org-publish-attachment)
         ("pages"
-         :components ("data_networks" "DGA" "Tools Usage" "Number Theory" "German Grammars" "index"))
+         :components ("cryptography" "data_networks" "DGA" "Tools Usage" "Number Theory" "German Grammars" "index"))
 	)
       )
 (org-publish "pages" t nil)
